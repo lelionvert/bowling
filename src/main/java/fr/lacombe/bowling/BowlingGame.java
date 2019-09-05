71d4dd57c55f08d33959bb06a30f7911e6979b80
Guillaume LASNE <guillaume.lasne@lacombedulionvert.fr>
2019-09-05 11:52:36 +0200

    Entire bowling game of gutter roll

---
+++ src/main/java/fr/lacombe/bowling/BowlingGame.java
@@ -0,0 +1,13 @@
+package fr.lacombe.bowling;
+
+public class BowlingGame {
+    public int getIntermediateScore() {
+        return 0;
+    }
+
+    public void roll(int pinsKnockedOnRoll) {}
+
+    public int Score() {
+        return 0;
+    }
+}
---
+++ src/test/java/fr/lacombe/bowling/BowlingGameTest.java
@@ -0,0 +1,30 @@
+package fr.lacombe.bowling;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BowlingGameTest {
+    @Test
+    void two_gutter_rolls_should_give_intermediate_score_of_0() {
+        int gutterRoll = 0;
+        BowlingGame bowlingGame = new BowlingGame();
+
+        bowlingGame.roll(gutterRoll);
+        bowlingGame.roll(gutterRoll);
+        int intermediateScore = bowlingGame.getIntermediateScore();
+
+        assertThat(intermediateScore).isEqualTo(0);
+    }
+
+    @Test
+    void renameMe() {
+        BowlingGame bowlingGame = new BowlingGame();
+        for (int i = 0; i < 20; i++) {
+            bowlingGame.roll(0);
+        }
+        int finalScore = bowlingGame.Score();
+
+        assertThat(finalScore).isEqualTo(0);
+    }
+}
